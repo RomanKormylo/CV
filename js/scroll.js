645135db24ea64af8e6bca3f1503333f7a35ad53
romankormylo <kormyloroman@yahoo.co.uk>
2016-03-05 20:57:17 +0000

    fixes in scroll and collaps js

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,11 @@
+$(document).ready(function(){
 var $root = $('html, body');
-$('a').click(function() {
+$('.navigation').click(function() {
     var href = $.attr(this, 'href');
     $root.animate({
         scrollTop: $(href).offset().top
     }, 400, function () {
         window.location.hash = href;
     });
-    return false;
+});
 });
